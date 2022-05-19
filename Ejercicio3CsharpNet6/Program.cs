ae6c871d993865e38107794804acb1e49453b7a6
Joana <Wooli3000@gmail.com>
2022-05-18 21:24:23 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio3CsharpNet6/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] numeros = new int[10];
+for (int i = 0; i < numeros.Length; i++)
+{
+    Console.Write($"Por favor, ingrese el {i + 1}º numero: ");
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine($"La suma de los numeros ingresados es: {numeros.Sum()}");
+Console.WriteLine($"El mayor de los numeros ingresados es: {numeros.Max()}");
+Console.WriteLine($"El menor de los numeros ingresados es: {numeros.Min()}");
+Console.WriteLine($"El promedio de todos los numeros ingresados es: {numeros.Average()}");
+Console.Write($"Los numeros ingresados fueron: ");
+for (int i = 0; i < numeros.Length; i++)
+{
+    Console.Write($"{numeros[i]} ");
+}
